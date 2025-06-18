219d9884229b85af5716eff9c3e4471ae5eaec7c
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-18 14:18:30 +0545

    created input field using native

--- src/LoginMagnus.tsx
+++ src/LoginMagnus.tsx
@@ -1,22 +1,58 @@
-import { View, Text } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, TextInput } from 'react-native';
 import React from 'react';
-import { Div, StatusBar, ThemeProvider } from 'react-native-magnus';
+import { Div, StatusBar, Text, ThemeProvider } from 'react-native-magnus';
 
 const theme = {
   colors: {
-    blue: '#5C95F8',
+    bgColor: '#5C95F8',
+    btnColor: '#51E6A6',
   },
 };
 
 const LoginMagnus = () => {
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar backgroundColor="#5C95F8" barStyle="light-content" />
-      <Div bg="#5C95F8" h="100%">
-        <Text>LoginMagnus</Text>
-      </Div>
+      <StatusBar backgroundColor="bgColor" barStyle="light-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Div
+          bg="bgColor"
+          h="100%"
+          flex={1}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Div>
+            <Text
+              color="white"
+              fontWeight="bold"
+              fontSize="5xl"
+              textAlign="center"
+              pb="xl"
+            >
+              Login
+            </Text>
+            {/* <TextInput placeholder="email" style={styles.input} /> */}
+          </Div>
+
+          <Div>
+            <Text>Hi</Text>
+          </Div>
+        </Div>
+      </SafeAreaView>
     </ThemeProvider>
   );
 };
 
 export default LoginMagnus;
+
+// const styles = StyleSheet.create({
+//   input: {
+//     width: Dimensions.get('window').width - 30,
+//     height: 40,
+//     borderRadius: 8,
+//     paddingHorizontal: 12,
+//     fontSize: 15,
+//     backgroundColor: 'white',
+//     color: 'black',
+//   },
+// });
