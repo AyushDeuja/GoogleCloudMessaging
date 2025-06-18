dd94bb75767c632249ae2bf88ac90dd822dcf75c
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-18 15:39:47 +0545

    splice ui created

--- src/Splice.tsx
+++ src/Splice.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Div, Image, ThemeProvider } from 'react-native-magnus';
+import { Div, Image, StatusBar, ThemeProvider } from 'react-native-magnus';
 
 const Splice = () => {
   return (
     <ThemeProvider>
-      <Div bg="bgColor">
+      <StatusBar backgroundColor="#5C95F8" barStyle="light-content" />
+      <Div
+        bg="#5C95F8"
+        h={'100%'}
+        flex={1}
+        justifyContent="center"
+        alignItems="center"
+      >
         <Image
           source={{
-            uri: 'https://i.fbcd.co/products/resized/resized-750-500/563d0201e4359c2e890569e254ea14790eb370b71d08b6de5052511cc0352313.jpg',
+            uri: 'https://www.shutterstock.com/image-vector/modern-vector-graphic-leaf-abstrack-600nw-1960180105.jpg',
           }}
           w={300}
           h={200}
