51700738d22b0d9587d87c819128bea53537cd8b
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-18 13:52:18 +0545

    status bar color changed to bg color

--- src/LoginMagnus.tsx
+++ src/LoginMagnus.tsx
@@ -1,11 +1,21 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { Div, StatusBar, ThemeProvider } from 'react-native-magnus';
+
+const theme = {
+  colors: {
+    blue: '#5C95F8',
+  },
+};
 
 const LoginMagnus = () => {
   return (
-    <View>
-      <Text>LoginMagnus</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar backgroundColor="#5C95F8" barStyle="light-content" />
+      <Div bg="#5C95F8" h="100%">
+        <Text>LoginMagnus</Text>
+      </Div>
+    </ThemeProvider>
   );
 };
 
