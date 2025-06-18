0aee6084c3db4ef9756b3c35e18a60d8b1fec235
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-18 15:34:55 +0545

    inserted image

--- src/Splice.tsx
+++ src/Splice.tsx
@@ -3,7 +3,18 @@ import { Div, Image, ThemeProvider } from 'react-native-magnus';
 
 const Splice = () => {
   return (
-    
+    <ThemeProvider>
+      <Div bg="bgColor">
+        <Image
+          source={{
+            uri: 'https://i.fbcd.co/products/resized/resized-750-500/563d0201e4359c2e890569e254ea14790eb370b71d08b6de5052511cc0352313.jpg',
+          }}
+          w={300}
+          h={200}
+          rounded="lg"
+        />
+      </Div>
+    </ThemeProvider>
   );
 };
 
