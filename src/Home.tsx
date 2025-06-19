f6998af42efd2d855f1cdba10e7f302af02a3e51
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-19 14:06:11 +0545

    axios peckage added

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View style={{ backgroundColor: 'white', height: '100%' }}>
+      <Text>This is home</Text>
+    </View>
+  );
+};
+
+export default Home;
